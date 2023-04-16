214d3ca10d3618f323298c3ced6575b641422f3d
omeralmali1 <omeralmali1@gmail.com>
2023-04-16 21:15:27 +0300

    Add project files.

---
+++ NumberConvertBinary/Program.cs
@@ -0,0 +1,13 @@
+﻿int enternumber = int.Parse(Console.ReadLine());
+double ascii=0;
+int i = 0;
+
+while (enternumber >= 2)
+{
+    ascii = ascii + (enternumber % 2) * (Math.Pow(10,i));
+    enternumber = enternumber / 2;
+    i++;
+}
+ascii=ascii+(enternumber* Math.Pow(10, i));   
+Console.WriteLine(ascii);
+Console.ReadLine();
